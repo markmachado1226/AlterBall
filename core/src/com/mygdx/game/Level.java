51f643ce61882b3839a8cefe87b33d324b660358
Shahroz <mugeneve@gmail.com>
2023-03-13 23:11:05 -0400

    Add tilemap and collisions for level 1

--- core/src/com/mygdx/game/Level.java
+++ core/src/com/mygdx/game/Level.java
@@ -2,19 +2,19 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.maps.MapRenderer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
-import java.util.concurrent.Future;
+
+public class Level implements Screen {
 
 
-
-public class Level1 implements Screen {
 
     private OrthographicCamera camera = new OrthographicCamera();
     private Box2DDebugRenderer debugRenderer;
@@ -22,40 +22,41 @@ public class Level1 implements Screen {
 
     TiledMap map = new TmxMapLoader().load("SampleMap.tmx");
 
-
     MyGdxGame game;
 
     BodyDef floorBodyDef;
     BodyDef playerBodyDef;
     float unitScale = 1/32f;
     OrthogonalTiledMapRenderer mapRenderer;
-    Level1 (MyGdxGame game) {
+
+    Viewport viewport;
+
+    Level (MyGdxGame game) {
 
         this.game = game;
-        camera.setToOrtho(false,1280f,720f);
         debugRenderer = new Box2DDebugRenderer();
-        mapRenderer = new OrthogonalTiledMapRenderer(map,unitScale);
+        mapRenderer = new OrthogonalTiledMapRenderer(map,1f);
+        camera.setToOrtho(false,1280f,720f);
+        camera.update();
 
-        floorBodyDef = new BodyDef();
+        System.out.println(camera.position.y);
+
+        camera.position.x = 160;
+        camera.position.y = 90;
+
+        viewport = new FitViewport(320,180,camera);
+
         playerBodyDef = new BodyDef();
 
-        floorBodyDef.type = BodyDef.BodyType.StaticBody;
-        Body floorBody = world.createBody(floorBodyDef);
-
-        playerBodyDef.position.set(500f,100f);
+        playerBodyDef.position.set(0f,40f);
         playerBodyDef.type = BodyDef.BodyType.DynamicBody;
 
         Body playerBody = world.createBody(playerBodyDef);
 
-        PolygonShape floorShape = new PolygonShape();
-        floorShape.setAsBox(camera.viewportWidth,10.0f);
-        floorBody.createFixture(floorShape,0.0f);
-
         CircleShape playerShape = new CircleShape();
-        playerShape.setRadius(20f);
+        playerShape.setRadius(10f);
 
         FixtureDef fixtureDef = new FixtureDef();
-
 
         fixtureDef.shape = playerShape;
         fixtureDef.density = 0.0f;
@@ -63,11 +64,10 @@ public class Level1 implements Screen {
         fixtureDef.restitution = 0.6f;
 
         Fixture fixture = playerBody.createFixture(fixtureDef);
-        floorShape.dispose();
         playerShape.dispose();
 
-        mapRenderer.setView(camera);
-
+        //Load level collisions
+        game.loadLevel(map,world);
 
     }
 
@@ -81,7 +81,7 @@ public class Level1 implements Screen {
     @Override
     public void render(float delta) {
 
-        ScreenUtils.clear(0,0,0.0f,1f);
+        ScreenUtils.clear(0,0,0f,1f);
 
         camera.update();
 
@@ -90,16 +90,18 @@ public class Level1 implements Screen {
         game.batch.begin();
 
         world.step(1/60f,6,2);
-        debugRenderer.render(world, camera.combined);
+
+        mapRenderer.setView(camera);
         mapRenderer.render();
 
+        debugRenderer.render(world, camera.combined);
         game.batch.end();
 
     }
 
     @Override
     public void resize(int width, int height) {
-
+    viewport.update(width,height);
     }
 
     @Override
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -4,22 +4,38 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.Game;
 
+
 import java.util.ArrayList;
+import java.util.Vector;
 
 public class MyGdxGame extends Game {
+
+	//Pixel level width and height of game
+	public final int width = 320;
+	public final int height = 180;
+
+	//Tile size
+	public final int tileSize = 32;
+
 
 	public SpriteBatch batch;
 	public Texture img;
 
 	private final String name;//the title of the game
 	private ArrayList<Player> players;// the players of the game
+
+	public Vector<Vector2> level1OffsetsCols = new Vector();
 
 	@Override
 	public void render () {
@@ -35,12 +51,15 @@ public class MyGdxGame extends Game {
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
-		this.setScreen(new Level1(this));
+		this.setScreen(new Level(this));
 	}
 
 	public MyGdxGame(String name) {
 		this.name = name;
 		players = new ArrayList();
+		level1OffsetsCols.add(new Vector2(width/2 - 80,16.0f));
+		level1OffsetsCols.add(new Vector2(64,16.0f));
+		level1OffsetsCols.add(new Vector2(27.7f,4.5f));
 	}
 
 	/**
@@ -78,19 +97,45 @@ public class MyGdxGame extends Game {
 
 	};
 
-	public void loadLevel(TiledMap tm, MapObjects objs, World world) {
-		for(int x = 0; x < tm.getLayers().size(); x++) {
+	public void loadLevel(TiledMap tm, World world) {
 			//If current layer is not a tiled layer (layer for collision boxes)
-			if(!(tm.getLayers().get(x) instanceof TiledMapTileLayer)) {
 				//Parse the objects and create the collision boxes
-				objs = tm.getLayers().get(x).getObjects();
-				for(int y = 0; y < objs.getCount(); y++) {
-					BodyDef colBox = new BodyDef();
-					colBox.type = BodyDef.BodyType.StaticBody;
+				MapObjects col = tm.getLayers().get("Collision").getObjects();
+				MapObjects colRotated = tm.getLayers().get("CollisionRotated").getObjects();
+
+				for(int y = 0; y < col.getCount(); y++) {
+					MapObject obj = col.get(y);
+					createBoxCollisions(obj,world,y,level1OffsetsCols);
 				}
+
+			for(int y = 0; y < colRotated.getCount(); y++) {
+				MapObject obj = colRotated.get(y);
+				createBoxCollisions(obj,world,level1OffsetsCols.size()-1 + y,level1OffsetsCols);
 			}
-		}
+
 	}
 
+	public void createBoxCollisions(MapObject obj,World world, int offsetIndex,Vector<Vector2> offsetVector) {
+		if(obj instanceof RectangleMapObject) {
+			RectangleMapObject rectangleMapObject = (RectangleMapObject) obj;
+			Rectangle rectangle = rectangleMapObject.getRectangle();
 
+			float xPos = rectangle.getX() + offsetVector.get(offsetIndex).x;
+			float yPos = rectangle.getY() + offsetVector.get(offsetIndex).y;
+
+			float angle = 0.0f;
+			angle = rectangleMapObject.getProperties().get("rotation",0.0f,Float.class);
+
+			BodyDef colBox = new BodyDef();
+			colBox.type = BodyDef.BodyType.StaticBody;
+			Body colBoxBody = world.createBody(colBox);
+
+			colBoxBody.setTransform(xPos,yPos,angle*8.5f);
+			PolygonShape polyShape = new PolygonShape();
+			polyShape.setAsBox(rectangle.getWidth()/2f, rectangle.getHeight()/2f);
+			colBoxBody.createFixture(polyShape,0.0f);
+			polyShape.dispose();
+
+		}
+	}
 }
